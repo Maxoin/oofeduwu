f2e2c53d7e488c8d11b43731dae109112f8c1681
Maxoin <39875172+Maxoin@users.noreply.github.com>
2020-07-05 14:51:07 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,7 +16,7 @@ bot.on('message', message => { //Garen
     message.channel.send("https://www.youtube.com/watch?v=5ddOT_TJnKg\n\n\n**Puisse l'__Honneur__ guider mes pas dans ce combat d'anthologie.**")
     }
     if(message.author.id === "395678267207843872" && message.content === "!!combat2"){
-    message.channel.send("https://www.youtube.com/watch?v=D-FH3VdSmig\n\n\n**Je donnerai tout pour répendre Justice.**")
+    message.channel.send("https://www.youtube.com/watch?v=b52jaQQFPj8\n\n\n**Je donnerai tout pour répendre Justice.**")
     }
     if(message.author.id === "395678267207843872" && message.content === "!!combat1"){
     message.channel.send("https://www.youtube.com/watch?v=aMxcPgEJkfA\n\n\n**Je m'occupe de lui et je reviens.**")
