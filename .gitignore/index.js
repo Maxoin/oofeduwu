fe233aff5feb39203b16b4251b638f8c2aeac3d5
Maxoin <39875172+Maxoin@users.noreply.github.com>
2020-07-06 11:17:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,10 +10,10 @@ bot.login(process.env.token);
 
 bot.on('message', message => { //Garen
     if(message.author.id === "395678267207843872" && message.content === "!!list"){
-    message.channel.send("__**~Garen~**__\n-karma\n-combat1\n-combat2\n-combat3\n-sad")
+    message.channel.send("__**~Garen~**__\n*Émotions :*\n``karma``, ``sad``\n*Combat :*\n``combat1``, ``combat2``, ``combat3``, ``berzerk``")
     }
     if(message.author.id === "395678267207843872" && message.content === "!!karma"){
-    message.channel.send("https://www.youtube.com/watch?v=2O7MmZ8geMw\n\n\n**Le Karma ? Je suis le Karma.**")
+    message.channel.send("https://www.youtube.com/watch?v=2O7MmZ8geMw\n\n\n**Si je suis la, c'est pour te faire payer ton erreur, qui a été de venir nous faire chier.**")
     }
     if(message.author.id === "395678267207843872" && message.content === "!!combat3"){
     message.channel.send("https://www.youtube.com/watch?v=5ddOT_TJnKg\n\n\n**Puisse l'__Honneur__ guider mes pas dans ce combat d'anthologie.**")
@@ -27,11 +27,14 @@ bot.on('message', message => { //Garen
     if(message.author.id === "395678267207843872" && message.content === "!!sad"){
     message.channel.send("https://www.youtube.com/watch?v=uOhaJNUhPG4\n\n\n**Seul, je ne tiendrai pas le coup.**")
     }
+    if(message.author.id === "395678267207843872" && message.content === "!!berzerk"){
+    message.channel.send("https://www.youtube.com/watch?v=uOhaJNUhPG4\n\n\n**C'est en levant la patte sur mes amis que tu as signé ton arret de mort.**")
+    }
 })
 
 bot.on('message', message => { //Des
     if(message.author.id === "161908030559092738" && message.content === "!!list"){
-    message.channel.send("__**~Des~**__\n-investigations1")
+    message.channel.send("__**~Des~**__\n*Situations :*\n``investigations1``")
     }
     if(message.author.id === "161908030559092738" && message.content === "!!investigations1"){
     message.channel.send("https://www.youtube.com/watch?v=AtgquM4JKR0")
@@ -40,7 +43,7 @@ bot.on('message', message => { //Des
 
 bot.on('message', message => { //Iris
     if(message.author.id === "339848621908426752" && message.content === "!!list"){
-    message.channel.send("__**~Iris~**__\n-sad\n-fight")
+    message.channel.send("__**~Iris~**__\n*Émotions :*``sad``\n*Combat :*\n``fight``")
     }
     if(message.author.id === "339848621908426752" && message.content === "!!sad"){
     message.channel.send("https://www.youtube.com/watch?v=D3scyfzqRU0")
